7eb29d16980c5e7c593123f8dc06b409fce8eaa3
frostyleave <1072295387@qq.com>
2025-10-01 11:04:17 +0800

    修错

--- web/static/js/auth-manager.js
+++ web/static/js/auth-manager.js
@@ -25,7 +25,7 @@ class AuthManager {
                 return;
             }
 
-            localStorage.setItem("token", response.data.get('access_token'))
+            localStorage.setItem("token", response.data.access_token)
 
             return 'ok'
         } catch (error) {
